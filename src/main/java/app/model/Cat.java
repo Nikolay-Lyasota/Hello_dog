3605cf426ec849f5624be2b0b459c853bc44f5e4
Nikolay-Lyasota <nikolaylyasota@gmail.com>
2021-06-11 23:27:37 +0300

    init commit

--- src/main/java/app/model/Cat.java
+++ src/main/java/app/model/Cat.java
@@ -5,7 +5,5 @@ import org.springframework.stereotype.Component;
 @Component
 public class Cat extends Animal {
     @Override
-    public String toString() {
-        return "Im a Cat";
-    }
+    public String toString() { return "Im a Cat"; }
 }
